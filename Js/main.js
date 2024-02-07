5638b4b597dfc6c029525432142c31fd32a19911
minimap <137304153+Khalid-Safi@users.noreply.github.com>
2024-02-07 23:20:59 +0300

    Update main.js

--- Js/main.js
+++ Js/main.js
@@ -20,30 +20,43 @@ getWeatherThanLocal()
 function getWeather(){
     let weather = `https://api.openweathermap.org/data/2.5/weather?q=${searchInput.value}&appid=b513826a8063ba4050422b7e5e7b1549`;
     localStorage.setItem('adress', weather);
-    fetch(localStorage.getItem('adress'))
+    fetch(weather)
         .then((response) => response.json())
-
         .then((repositories) => {
-            if(repositories.cod == '404'){
+            
+            if(repositories.cod == '404' || searchInput.value == ' '){
                 searchInput.classList.add('error');
                 setTimeout(() => {
                     searchInput.classList.remove('error')
                 }, 1000);
             }
             else{
-                city.innerHTML = repositories.name;
-                let celisius = repositories.main.temp - 273;
-                derece.innerHTML = `<h1 id="derece">${Math.floor(celisius)} <span>o</span>  C</h1>`;
-                weatherCity.innerHTML = `${repositories.weather[0].main}(${repositories.weather[0].description})`
-                wendCity.innerHTML  = `${repositories.wind.speed}km/H Wind Speed`
-                icon.src = `https://openweathermap.org/img/wn/${repositories.weather[0].icon}.png`;
-                humidity.innerHTML  = `${repositories.main.humidity}% humidity`
 
+                if(repositories.sys.country == 'IL'){
+                        city.innerHTML = `free palestine`;
+                        searchInput.classList.add('error');
+                    setTimeout(() => {
+                        searchInput.classList.remove('error')
+                    }, 1000);
+                    localStorage.removeItem('adress');
+                }
+
+                else{
+                    city.innerHTML = repositories.name;
+                    let celisius = repositories.main.temp - 273;
+                    derece.innerHTML = `<h1 id="derece">${Math.floor(celisius)} <span>o</span>  C</h1>`;
+                    weatherCity.innerHTML = `${repositories.weather[0].main}(${repositories.weather[0].description})`
+                    wendCity.innerHTML  = `${repositories.wind.speed}km/H Wind Speed`
+                    icon.src = `https://openweathermap.org/img/wn/${repositories.weather[0].icon}.png`;
+                    humidity.innerHTML  = `${repositories.main.humidity}% humidity`
+                    
+                }
             }
-            
+         
         });
     
-    
+        
+    searchInput.value = '';
 
 }
 function getWeatherThanLocal(){
@@ -55,7 +68,7 @@ function getWeatherThanLocal(){
             let celisius = repositories.main.temp - 273;
             derece.innerHTML = `<h1 id="derece">${Math.floor(celisius)} <span>o</span>  C</h1>`;
             weatherCity.innerHTML = `${repositories.weather[0].main}(${repositories.weather[0].description})`
-            wendCity.innerHTML  = `${repositories.wind.speed}km/H Wind Speed`
+            wendCity.innerHTML  = `${repositories.wind.speed}km/h Wind Speed`
             icon.src = `https://openweathermap.org/img/wn/${repositories.weather[0].icon}.png`;
             humidity.innerHTML  = `${repositories.main.humidity}% humidity`
         });
